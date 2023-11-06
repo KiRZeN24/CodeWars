a3e0bed0ffd0fd21bd051e478395c0015c5b1ad5
KiRZeN24 <carlosrufo92@gmail.com>
2023-11-06 11:34:48 +0100

    Añadido el ejercicio String ends with

--- String ends with.js
+++ String ends with.js
@@ -1,7 +1,11 @@
-function solution(str, ending) {
-  // Usamos el método slice para obtener una porción de 'str' con la misma longitud que 'ending' desde el final.
-  // Luego, comparamos si esa porción es igual a 'ending'.
-  if (str.slice(-ending.length) === ending) {
+function solution(str, ending){
+    if (ending === '') {
+    return true; // Si 'ending' es una cadena vacía, devolvemos true.
+  }
+  
+    // Usamos el método slice para obtener una porción de 'str' con la misma longitud que 'ending' desde el final.
+    // Luego, comparamos si esa porción es igual a 'ending'.
+    if (str.slice(-ending.length) === ending) {
     return true; // Si son iguales, devolvemos true.
   } else {
     return false; // Si no son iguales, devolvemos false.
