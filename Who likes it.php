f210a51116856db3ca9b307a4b780269532ed986
KiRZeN24 <carlosrufo92@gmail.com>
2023-11-13 11:29:12 +0100

    Añadida Kata Who likes it?

--- Who likes it.php
+++ Who likes it.php
@@ -1,23 +1,22 @@
-function likes( $names ) {
+<?php
 
-if (empty($names)) {
-  
-  return "no one likes this";
-  
-} elseif (count($names) == 1) {
-  
-  return $names[0] . " likes this";
-  
-} elseif (count($names) == 2) {
-  
-  return $names[0] . " and " . $names[1] . " like this";
-  
-} elseif (count($names) == 3) {
-  
-  return $names[0] . ", " . $names[1] . " and " . $names[2] . " like this";
-  
-} else {
-  return $names[0] . ", " . $names[1] . " and " . (count($names) -2) . " others like this";
-}
+function likes($names)
+{
 
+  if (empty($names)) {
+
+    return "no one likes this";
+  } elseif (count($names) == 1) {
+
+    return $names[0] . " likes this";
+  } elseif (count($names) == 2) {
+
+    return $names[0] . " and " . $names[1] . " like this";
+  } elseif (count($names) == 3) {
+
+    return $names[0] . ", " . $names[1] . " and " . $names[2] . " like this";
+  } else {
+    return $names[0] . ", " . $names[1] . " and " . (count($names) - 2) . " others like this";
+  }
 }
+?>
